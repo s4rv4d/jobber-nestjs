0b591d32dbfbbb9542aa04f3fea9eda81084cabd
serverConnected <sarvadshetty@gmail.com>
2025-12-22 12:55:38 +0300

    setup grpc transport config and updated nx file to include proto data in dist

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -4,11 +4,15 @@
  */
 
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +22,17 @@ async function bootstrap() {
   // add cookie parser middleware
   app.use(cookieParser());
   const port = app.get(ConfigService).getOrThrow('PORT');
+
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+
+  // listen for http and grpc reqs
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
