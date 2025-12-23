8f04cd21207b50a6d3819cc6f7c03579b147d577
serverConnected <sarvadshetty@gmail.com>
2025-12-23 13:53:03 +0300

    rearranged files

--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { FibonacciJob } from './fibonacci.job';
+import { FibonacciJob } from './fibonacci/fibonacci.job';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
