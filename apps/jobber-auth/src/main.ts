41e4db67a2f03634f36d35c1500ae7f2e905fa12
serverConnected <sarvadshetty@gmail.com>
2025-12-23 15:25:22 +0300

    refactored code

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -1,27 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { init } from '@jobber/nestjs';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
-import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
-import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix(globalPrefix);
-  // add cookie parser middleware
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
+  await init(app);
 
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
@@ -31,12 +19,8 @@ async function bootstrap() {
     },
   });
 
-  // listen for http and grpc reqs
+  // listen for grpc reqs
   await app.startAllMicroservices();
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
 }
 
 bootstrap();
--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -1,26 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+import { init } from '@jobber/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix(globalPrefix);
-  // add cookie parser middleware
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  await init(app);
 }
 
 bootstrap();
