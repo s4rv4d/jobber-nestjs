560966fefdd35387b31f4ef9d10e2fd47dae6aec
serverConnected <sarvadshetty@gmail.com>
2026-01-03 22:37:08 +0300

    updated job module

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -9,19 +9,24 @@ import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { PulsarModule } from '@jobber/pulsar';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DiscoveryModule,
     PulsarModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
-        },
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+            package: AUTH_PACKAGE_NAME,
+            protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
